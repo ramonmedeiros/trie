cd525c14f965291b0aae26177df9acfc43fa4065
Ramon Medeiros <ramon.rnm@gmail.com>
2024-05-06 18:43:03 +0200

    fixing logic
    - save char at each node of the Trie
    
    - concatenate chars when searching for prefix

--- main.scala
+++ main.scala
@@ -4,19 +4,21 @@ def filename = "sample_prefixes.txt"
 
 class TrieNode {
   val children = new Array[TrieNode](50)
-  var value = ""
+  var value = " ".charAt(0) 
 }
 
 def constructTrie(root: TrieNode): Unit = {
   val source = Source.fromFile(filename)
   val lines = source.getLines
   for (word <- lines) {
-    var node = new TrieNode
+    var node = root
     for (c <- word) {
       val calc = (c - 'a').abs
-      if (node.children(calc) == null) node.children(calc) = new TrieNode
-          node = node.children(calc)
-      node.value = word
+      if (node.children(calc) == null){
+        node.children(calc) = new TrieNode
+        node.value = c
+      }
+      node = node.children(calc)
     }
   }
   source.close()
@@ -24,13 +26,15 @@ def constructTrie(root: TrieNode): Unit = {
 
 def findWord(word: String, root: TrieNode): String = {
   var node = root
+  var prefix = ""
   for (c <- word) {
     val calc = (c - 'a').abs
-    if (node.children(calc) != null && node.children(calc).value.nonEmpty) {
+    if (node.children(calc) != null) {
       node = node.children(calc)
+      prefix = prefix + c
     }
   }
-  node.value
+  prefix
 }
 
 @main def main(word: String): Unit = {
